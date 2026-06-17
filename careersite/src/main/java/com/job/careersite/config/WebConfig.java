52797bcba507ab485abcf22207cfb02fcb4da14b
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2026-06-18 00:35:11 +0530

    new2

--- careersite/src/main/java/com/job/careersite/config/WebConfig.java
+++ careersite/src/main/java/com/job/careersite/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173", "https://careersitespring-6.onrender.com")
+            .allowedOrigins("http://localhost:5173", "https://shimmering-liger-862c4b.netlify.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
