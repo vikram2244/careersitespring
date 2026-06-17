ef4e96e159d8a8a0c0fc3534dff1e1f2f19e8eeb
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2026-06-18 00:40:55 +0530

    Update CrossOrigin to include Netlify URL

--- careersite/src/main/java/com/job/careersite/controllerjob/JobController.java
+++ careersite/src/main/java/com/job/careersite/controllerjob/JobController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://shimmering-liger-862c4b.netlify.app/"})
 public class JobController {
     
     @Autowired
