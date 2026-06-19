dd7576a5aa5c5f55b581095cc4d0415ef77a7584
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2026-06-19 10:02:10 +0530

    Update Company.java

--- careersite/src/main/java/com/job/careersite/usersentity/Company.java
+++ careersite/src/main/java/com/job/careersite/usersentity/Company.java
@@ -24,7 +24,7 @@ public class Company {
     @Transient
     private List<Job> jobs;
     
-    // Getters and Setters
+  
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
