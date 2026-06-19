a8824cff4ba70658478553ba6f33d06041488266
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2026-06-19 10:01:55 +0530

    Update Category.java

--- careersite/src/main/java/com/job/careersite/usersentity/Category.java
+++ careersite/src/main/java/com/job/careersite/usersentity/Category.java
@@ -21,7 +21,7 @@ public Category() {}
         this.icon = icon;
     }
     
-    // Getters and Setters
+  
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
